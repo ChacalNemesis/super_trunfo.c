d0e163ddf39ca6a7e0e291924cc099dc3cfe43a3
ChacalNemesis <allan.prestes1@gmail.com>
2025-04-28 17:12:42 -0300

    Super_trunfo.c

---
+++ Super_trunfo.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(){
+
+    int carta;
+    char estado[20];
+    char codigo[30]; 
+    char nome_da_cidade[40];
+    float populacao;
+    float area;
+    float pib;
+    int numero_de_pontos_turisticos;
+    printf("carta:");
+    scanf("%d", &carta);
+    printf("Estado:");
+    scanf("%s", &estado);
+    printf("codigo:");
+    scanf("%s", &codigo);
+    printf("nome_da_cidade:");
+    scanf("%s", &nome_da_cidade);
+    printf("populacao:");
+    scanf("%f", &populacao);
+    printf("area:");
+    scanf("%f", &area);
+    printf("pib:");
+    scanf("%f", &pib);
+    printf("numero_de_pontos_turisticos:");
+    scanf("%d", &numero_de_pontos_turisticos);
+    int carta2;
+    char estado2[20];
+    char codigo2[30]; 
+    char nome_da_cidade2[40];
+    float populacao2;
+    float area2;
+    float pib2;
+    int numero_de_pontos_turisticos2;
+    printf("carta2:");
+    scanf("%d", &carta2);
+    printf("Estado2:");
+    scanf("%s", &estado2);
+    printf("codigo2:");
+    scanf("%s", &codigo2);
+    printf("nome_da_cidade2:");
+    scanf("%s", &nome_da_cidade2);
+    printf("populacao2:");
+    scanf("%f", &populacao2);
+    printf("area2:");
+    scanf("%f", &area2);
+    printf("pib2:");
+    scanf("%f", &pib2);
+    printf("numero_de_pontos_turisticos2:");
+    scanf("%d", &numero_de_pontos_turisticos2);
+    return 0;
+}
